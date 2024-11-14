8d068f929b9eef2e01e4b75bed88b2c086bdf5ae
Shubham Tewari <112378956+st321123@users.noreply.github.com>
2024-11-14 21:17:43 +0530

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 import { RingLoader } from 'react-spinners';
 
-export function LoadingSpinner() {
+export default function LoadingSpinner() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-50">
       <div className="p-4 bg-white rounded-full shadow-md">
