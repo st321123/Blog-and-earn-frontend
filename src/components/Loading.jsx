004adf95851cc8e827273422a015579b78197bf0
Shubham Tewari <112378956+st321123@users.noreply.github.com>
2024-11-14 21:18:51 +0530

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,12 @@
-import { RingLoader } from 'react-spinners';
+import React from 'react';
+import { ClipLoader } from 'react-spinners';
 
-export default function LoadingSpinner() {
+function LoadingSpinner() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-50">
-      <div className="p-4 bg-white rounded-full shadow-md">
-        <RingLoader color="#00BFFF" size={60} loading={true} />
-      </div>
+    <div className="spinner-container">
+      <ClipLoader color="#00BFFF" size={50} loading={true} />
     </div>
   );
 }
+
+export default LoadingSpinner;
